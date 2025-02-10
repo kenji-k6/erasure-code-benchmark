baa5db9ad9f0c4cf8c07310e10cb56c0a18b0116
Kenji Yuri Nakano <kenji.y.nakano@gmail.com>
2025-02-10 19:04:04 +0100

    Leopard compiles, set up the CMakeLists.txt file

--- benchmark/benchmark_runner.cpp
+++ benchmark/benchmark_runner.cpp
@@ -2,6 +2,19 @@
 #include <chrono>
 #include "leopard.h"
 
+
+void benchmark_leopard() {
+  // Original and recovery data must not exceed 65536 pieces
+  // recovery_count <= original_count
+  // buffer_bytes has to be a multiple of 64
+  // each buffer should have same no. of bytes
+  // even the last piece must be rounded up to the block size
+
+  leo_init();
+}
+
+
 int main() {
+  benchmark_leopard();
   return 0;
 }
