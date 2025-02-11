4e7cb3ff9f426c2624bdf2e8bb3f4d694d9bbfaa
Kenji Yuri Nakano <kenji.y.nakano@gmail.com>
2025-02-11 16:28:17 +0100

    Working Compilation of all libraries

--- benchmark/benchmark_runner.cpp
+++ benchmark/benchmark_runner.cpp
@@ -1,12 +1,49 @@
+#include "leopard.h"
+#include "cm256.h"
+#include "wirehair/wirehair.h"
+#include "aff3ct.hpp"
+#include "erasure_code.h"
+
 #include <iostream>
 #include <chrono>
 
-
-void benchmark_leopard() {
+void benchmark_aff3ct() {
+  std::cout << "aff3ct Compiled!" << '\n';
 }
 
-int main() {
 
+void benchmark_cm256() {
+  if (cm256_init()) {
+    exit(1);
+  }
+  std::cout << "cm256 Compiled!" << '\n';
+}
+
+
+void benchmark_leopard() {
+  if (leo_init()) {
+    exit(1);
+  }
+  std::cout << "Leopard Compiled!" << '\n';
+}
+
+
+void benchmark_wirehair() {
+  const WirehairResult initResult = wirehair_init();
+  if (initResult != Wirehair_Success) {
+    exit(1);
+  }
+  std::cout << "Wirehair Compiled!" << '\n';
+}
+
+
+
+
+int main() {
+  benchmark_leopard();
+  benchmark_cm256();
+  benchmark_wirehair();
+  benchmark_aff3ct();
   std::cout << "All libraries compiled!" << '\n';
 
   return 0;
