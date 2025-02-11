7615594847f6dfa5d7d9f4c4a7fde0ad740151ba
Kenji Yuri Nakano <kenji.y.nakano@gmail.com>
2025-02-11 16:45:55 +0100

    First steps in actual Benchamrk part

--- benchmark/benchmark_runner.cpp
+++ benchmark/benchmark_runner.cpp
@@ -1,49 +1,18 @@
-#include "leopard.h"
-#include "cm256.h"
-#include "wirehair/wirehair.h"
-#include "aff3ct.hpp"
-#include "erasure_code.h"
-
-#include <iostream>
-#include <chrono>
-
-void benchmark_aff3ct() {
-  std::cout << "aff3ct Compiled!" << '\n';
-}
+#include "benchmark_runner.h"
 
 
-void benchmark_cm256() {
-  if (cm256_init()) {
-    exit(1);
-  }
-  std::cout << "cm256 Compiled!" << '\n';
-}
 
-
-void benchmark_leopard() {
+int benchmark_leopard() {
   if (leo_init()) {
-    exit(1);
+    // error in initialization
+    return 1;
   }
-  std::cout << "Leopard Compiled!" << '\n';
+
+  return 0;
 }
-
-
-void benchmark_wirehair() {
-  const WirehairResult initResult = wirehair_init();
-  if (initResult != Wirehair_Success) {
-    exit(1);
-  }
-  std::cout << "Wirehair Compiled!" << '\n';
-}
-
-
 
 
 int main() {
-  benchmark_leopard();
-  benchmark_cm256();
-  benchmark_wirehair();
-  benchmark_aff3ct();
   std::cout << "All libraries compiled!" << '\n';
 
   return 0;
---
+++ benchmark/benchmark_runner.h
@@ -0,0 +1,13 @@
+#ifndef BENCHMARK_RUNNER_H
+#define BENCHMARK_RUNNER_H
+
+#include "aff3ct.hpp"
+#include "cm256.h"
+#include "erasure_code.h" // ISA-L
+#include "leopard.h"
+#include "wirehair/wirehair.h"
+
+#include <iostream>
+#include <chrono>
+
+#endif
