13db9acf89e19162a20fd798c37f070092aff5bd
Kenji Yuri Nakano <kenji.y.nakano@gmail.com>
2025-02-11 12:55:30 +0100

    Temp Save before going back

--- benchmark/benchmark_runner.cpp
+++ benchmark/benchmark_runner.cpp
@@ -1,20 +1,52 @@
+// #include "aff3ct.hpp"
+#include "cm256.h"
+#include "gnuradio/fec/encoder.h"
+#include "gnuradio/fec/decoder.h"
+
+#include "leopard.h"
+#include "wirehair/wirehair.h"
+
 #include <iostream>
 #include <chrono>
-#include "leopard.h"
 
 
 void benchmark_leopard() {
-  // Original and recovery data must not exceed 65536 pieces
-  // recovery_count <= original_count
-  // buffer_bytes has to be a multiple of 64
-  // each buffer should have same no. of bytes
-  // even the last piece must be rounded up to the block size
+  if (leo_init()) {
+    // error if leo_init doesn't return 0
+    exit(1);
+  }
+  std::cout << "Sucess compiling leopard!" << '\n';
+}
 
-  leo_init();
+
+void benchmark_cm256() {
+  if (cm256_init()) {
+    // error if cm256_init doesn't return 0
+    exit(1);
+  }
+  std::cout << "Sucess compiling cm256!" << '\n';
+}
+
+
+void benchmark_wirehair() {
+  const WirehairResult initResult = wirehair_init();
+  if (initResult != Wirehair_Success) {
+    exit(1);
+  }
+  std::cout << "Sucess compiling Wirehair!" << '\n';
+}
+
+
+void benchmark_gnuradio() {
+  std::cout << "Todo: Compile GnuradioFEC" << '\n';
 }
 
 
 int main() {
   benchmark_leopard();
+  benchmark_cm256();
+  benchmark_wirehair();
+  benchmark_gnuradio();
+
   return 0;
 }
