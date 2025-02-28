418ba26afabc2e2962823d0d3ffdbc0c8010fad0
Kenji Yuri Nakano <kenji.y.nakano@gmail.com>
2025-02-28 18:31:32 +0100

    Temp save for today

--- scripts/plot_benchmark_results.py
+++ scripts/plot_benchmark_results.py
@@ -30,6 +30,8 @@ def plot_scatter(dfs: dict[int, pd.DataFrame], x_col: str, y_col: str, x_label: 
   plt.legend(title="Libraries", bbox_to_anchor=(1.05, 1), loc="upper left")
   plt.title(get_plot_title(dfs, plot_id), fontsize=12)
 
+  plt.errorbar(df[x_col], df[y_col], yerr=[df[f"{y_col}_lower"], df[f"{y_col}_upper"]], fmt='o')
+
   ax = plt.gca()
   x_ticks = sorted(df[x_col].unique())
   ax.set_xticks(x_ticks)
@@ -39,7 +41,7 @@ def plot_scatter(dfs: dict[int, pd.DataFrame], x_col: str, y_col: str, x_label: 
   plt.savefig(os.path.join(OUTPUT_DIR, file_name))
   plt.close()
 
-
+  
 
 def get_plot_title(dfs: dict[int, pd.DataFrame], plot_id:int):
   """Generate a plot title containing all the constant parameters."""
@@ -74,8 +76,7 @@ def process_dataframe(df: pd.DataFrame):
   df["time_ms"] = df["time_ns"] / 1e6
   df["tot_data_size_MiB"] = df["tot_data_size_B"] // (1024 * 1024)
   df["throughput_Gbps"] = (df["tot_data_size_B"] * 8) / df["time_ns"]
-  df["num_data_blocks"] = df["tot_data_size_B"] // df["block_size_B"]
-  df["num_parity_blocks"] = (df["redundancy_ratio"] * df["num_data_blocks"]).astype(int)
+
 
   # Return DataFrame grouped by plot_id
   # return {plot_id: group for plot_id, group in df.groupby("plot_id")}
@@ -101,27 +102,42 @@ if __name__ == "__main__":
 
   # Get additional columns 
   df["time_ms"] = df["time_ns"] / 1e6
+  df["encode_time_ms"] = df["encode_time_ns"] / 1e6
+  df["decode_time_ms"] = df["decode_time_ns"] / 1e6
   df["tot_data_size_MiB"] = df["tot_data_size_B"] // (1024 * 1024)
   df["throughput_Gbps"] = (df["tot_data_size_B"] * 8 / 1e9) / (df["time_ns"] / 1e9)
 
-  dfs = {plot_id: group for plot_id, group in df.groupby("plot_id")}
 
-  # X: Buffer Size, Y: Time
-  plot_scatter(dfs, "tot_data_size_MiB", "time_ms", "Buffer Size (MiB)", "Time (ms)", "buffer_size_vs_time.png", 0)
+  # Compute the confidence  intervals per row for the throughput and time columns
+  z = 1.96 # 95% confidence interval
 
-  # X: Buffer Size, Y: Throughput
-  plot_scatter(dfs, "tot_data_size_MiB", "throughput_Gbps", "Buffer Size (MiB)", "Throughput (Gbps)", "buffer_size_vs_throughput.png", 0)
+  df["encode_time_ms_lower"] = (df["encode_time_ns"] - z * (df["encode_time_stddev_ns"] / math.sqrt(df["num_iterations"]))) / 1e6
+  df["encode_time_ms_upper"] = (df["encode_time_ns"] + z * (df["encode_time_stddev_ns"] / math.sqrt(df["num_iterations"]))) / 1e6
+
+  df["decode_time_ms_lower"] = (df["decode_time_ns"] - z * (df["decode_time_stddev_ns"] / math.sqrt(df["num_iterations"]))) / 1e6
+  df["decode_time_ms_upper"] = (df["decode_time_ns"] + z * (df["decode_time_stddev_ns"] / math.sqrt(df["num_iterations"]))) / 1e6
+
+  
+    
+
+  # dfs = {plot_id: group for plot_id, group in df.groupby("plot_id")}
+
+  # # X: Buffer Size, Y: Time
+  plot_scatter(dfs, "tot_data_size_MiB", "encode_time_ms", "Buffer Size (MiB)", "Encode Time (ms)", "sdasdasdbuffer_size_vs_time.png", 0)
+
+  # # X: Buffer Size, Y: Throughput
+  # plot_scatter(dfs, "tot_data_size_MiB", "throughput_Gbps", "Buffer Size (MiB)", "Throughput (Gbps)", "buffer_size_vs_throughput.png", 0)
 
 
-  # X: Num Parity Blocks, Y: Time
-  plot_scatter(dfs, "num_parity_blocks", "time_ms", "#Parity Blocks", "Time (ms)", "parity_blocks_vs_time.png", 1)
+  # # X: Num Parity Blocks, Y: Time
+  # plot_scatter(dfs, "num_parity_blocks", "time_ms", "#Parity Blocks", "Time (ms)", "parity_blocks_vs_time.png", 1)
 
-  # X: Num Parity Blocks, Y: Throughput
-  plot_scatter(dfs, "num_parity_blocks", "throughput_Gbps", "#Parity Blocks", "Throughput (Gbps)", "parity_blocks_vs_throughput.png", 1)
+  # # X: Num Parity Blocks, Y: Throughput
+  # plot_scatter(dfs, "num_parity_blocks", "throughput_Gbps", "#Parity Blocks", "Throughput (Gbps)", "parity_blocks_vs_throughput.png", 1)
 
 
-  # X: Num Lost Blocks, Y: Time
-  plot_scatter(dfs, "num_lost_blocks", "time_ms", "#Lost Blocks", "Time (ms)", "lost_blocks_vs_time.png", 2)
+  # # X: Num Lost Blocks, Y: Time
+  # plot_scatter(dfs, "num_lost_blocks", "time_ms", "#Lost Blocks", "Time (ms)", "lost_blocks_vs_time.png", 2)
 
-  # X: Num Lost Blocks, Y: Throughput
-  plot_scatter(dfs, "num_lost_blocks", "throughput_Gbps", "#Lost Blocks", "Throughput (Gbps)", "lost_blocks_vs_throughput.png", 2)
+  # # X: Num Lost Blocks, Y: Throughput
+  # plot_scatter(dfs, "num_lost_blocks", "throughput_Gbps", "#Lost Blocks", "Throughput (Gbps)", "lost_blocks_vs_throughput.png", 2)
