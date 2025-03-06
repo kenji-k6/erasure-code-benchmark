c4b298f7deb2518ec20fa2011f7a4f602f87e360
Kenji Yuri Nakano <kenji.y.nakano@gmail.com>
2025-03-06 14:06:49 +0100

    Finished plotting of theoretical min/maxs

--- scripts/plot_benchmark_results.py
+++ scripts/plot_benchmark_results.py
@@ -1,4 +1,5 @@
 import os
+from webbrowser import get
 import cpuinfo
 import platform
 import psutil
@@ -20,6 +21,11 @@ LOG_OUTPUT_DIR = os.path.join(SCRIPT_DIR, "../results/processed/log/")
 # AVX XOR / AVX2 XOR max throughput
 AVX_XOR_CPI_XEON = 0.33
 AVX2_XOR_CPI_XEON = 0.33
+AVX_BITS = 128
+AVX2_BITS = 256
+AVX_COLOR = "cornflowerblue"
+AVX2_COLOR = "indigo"
+
 CPUInfo = namedtuple("CPUInfo", ["model_name", "clock_speed_GHz", "l1_cache_size_KiB", "l2_cache_size_KiB", "l3_cache_size_KiB"])
 
 class AxType(Enum):
@@ -114,7 +120,7 @@ def get_ax_label(ax: AxType) -> str:
   }
   return ax_map[ax]
 
-def get_min_encode_time(xor_cpi: float, avx_bits: int, cpu_frequency_GHz: float, data_size_B: int, num_data_blocks: int) -> float:
+def get_min_encode_time(xor_cpi: float, avx_bits: int, cpu_frequency_GHz: float, data_size_B: int, num_data_blocks: int, num_parity_blocks: int, num_lost_data_blocks: int) -> float:
   """Compute the theoretical minimum encoding time (single threaded)."""
   block_size_bits = (data_size_B // num_data_blocks) * 8
 
@@ -124,8 +130,9 @@ def get_min_encode_time(xor_cpi: float, avx_bits: int, cpu_frequency_GHz: float,
   return encode_time_ms
 
 
-def get_encode_throughput(encode_time_ms, data_size_B: int) -> float:
+def get_max_encode_throughput(xor_cpi: float, avx_bits: int, cpu_frequency_GHz: float, data_size_B: int, num_data_blocks: int, num_parity_blocks: int, num_lost_data_blocks: int) -> float:
   """Compute the theoretical maximum encoding throughput (single threaded)."""
+  encode_time_ms = get_min_encode_time(xor_cpi, avx_bits, cpu_frequency_GHz, data_size_B, num_data_blocks, num_parity_blocks, num_lost_data_blocks)
   data_size_bits = data_size_B * 8
   encode_throughput_Gbps = (data_size_bits / 1e6) / encode_time_ms # equal to (#bits/1e9)/(#ms/1e3) = #Gbps
   return encode_throughput_Gbps
@@ -135,18 +142,26 @@ def get_min_decode_time(xor_cpi: float, avx_bits: int, cpu_frequency_GHz: float,
   """Compute the theoretical minimum decoding time (single threaded)."""
   block_size_bits = (data_size_B // num_data_blocks) * 8
   blocks_per_parity_block = num_data_blocks // num_parity_blocks
-  num_xors_per_lost_data_block = ((blocks_per_parity_block - 1) * block_size_bits)
+  num_xors_per_lost_data_block = ((blocks_per_parity_block) * block_size_bits) / avx_bits
   tot_num_xors = num_xors_per_lost_data_block * num_lost_data_blocks
   num_cycles = tot_num_xors * xor_cpi
   decode_time_ms = (num_cycles / cpu_frequency_GHz) / 1e6 # equal to (#cycles / (#GHz * 1e9)) * 1e3  = (#cycles / #Hz) * 1e3
   return decode_time_ms
 
 
-def get_decode_throughput(decode_time_ms, data_size_B: int) -> float:
+def get_max_decode_throughput(xor_cpi: float, avx_bits: int, cpu_frequency_GHz: float, data_size_B: int, num_data_blocks: int, num_parity_blocks: int, num_lost_data_blocks: int) -> float:
   """Compute the theoretical maximum decoding throughput (single threaded)."""
+  decode_time_ms = get_min_decode_time(xor_cpi, avx_bits, cpu_frequency_GHz, data_size_B, num_data_blocks, num_parity_blocks, num_lost_data_blocks)
   data_size_bits = data_size_B * 8
   decode_throughput_Gbps = (data_size_bits / 1e6) / decode_time_ms # equal to (#bits/1e9)/(#ms/1e3) = #Gbps
   return decode_throughput_Gbps
+
+avx_avx2_func = {
+  AxType.ENCODE_T: get_min_encode_time,
+  AxType.DECODE_T: get_min_decode_time,
+  AxType.ENCODE_TP: get_max_encode_throughput,
+  AxType.DECODE_TP: get_max_decode_throughput
+}
 
 def plot_cache_sizes(cpu_info: CPUInfo) -> None:
   l2_cache_size = cpu_info.l2_cache_size_KiB
@@ -179,6 +194,38 @@ def plot_confidence_intervals(df: pd.DataFrame, x_ax: AxType, y_ax: AxType) -> N
                  fmt='o', color=ax.get_legend().legend_handles[df["name"].unique().tolist().index(name)].get_color(), capsize=5 
     )
 
+def plot_avx_avx2_xor(df: pd.DataFrame, x_ax: AxType, y_ax: AxType, cpu_frequency_GHz: float) -> None:
+  x_col = get_col_name(x_ax)
+  y_col = get_col_name(y_ax)
+  f = avx_avx2_func[y_ax]
+
+  params_df = df[[
+    "tot_data_size_KiB",
+    "num_data_blocks",
+    "num_parity_blocks",
+    "num_lost_blocks"
+  ]].drop_duplicates().sort_values(by=x_col, ascending=True)
+
+  x_values = params_df[x_col].unique()
+
+  y_values_AVX = params_df.apply(
+    lambda row: f(AVX_XOR_CPI_XEON, AVX_BITS, cpu_frequency_GHz, row["tot_data_size_KiB"]*1024,
+                  row["num_data_blocks"], row["num_parity_blocks"], row["num_lost_blocks"]
+                  ),
+    axis=1
+  ).tolist()
+
+  y_values_AVX2 = params_df.apply(
+    lambda row: f(AVX2_XOR_CPI_XEON, AVX2_BITS, cpu_frequency_GHz, row["tot_data_size_KiB"]*1024,
+                  row["num_data_blocks"], row["num_parity_blocks"], row["num_lost_blocks"]
+                  ),
+    axis=1
+  ).tolist()
+
+  assert(len(x_values) == len(y_values_AVX) == len(y_values_AVX2))
+  plt.plot(x_values, y_values_AVX, label="AVX XOR", color=AVX_COLOR, linestyle="--")
+  plt.plot(x_values, y_values_AVX2, label="AVX2 XOR", color=AVX2_COLOR, linestyle="--")
+
 
 def write_scatter_plot(dfs: Dict[int, pd.DataFrame], x_ax: AxType, y_ax: AxType, y_scale: str, plot_id: int) -> None:
   """Generate and save scatter plot."""
@@ -202,6 +249,7 @@ def write_scatter_plot(dfs: Dict[int, pd.DataFrame], x_ax: AxType, y_ax: AxType,
   plt.yscale(y_scale)
 
   if x_ax == AxType.BUF_SIZE: plot_cache_sizes(cpu_info)
+  plot_avx_avx2_xor(df, x_ax, y_ax, cpu_info.clock_speed_GHz)
 
   plt.legend(title="Libraries", bbox_to_anchor=(1.05, 1), loc="upper left")
   plt.title(get_plot_title(df, plot_id, cpu_info), fontsize=12)
@@ -288,4 +336,4 @@ if __name__ == "__main__":
 
   for params in plot_params:
     write_scatter_plot(dfs, *params)
-
+  
