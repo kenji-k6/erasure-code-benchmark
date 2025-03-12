1227e52f230553b96adced246b8922d29bb6e0a4
Kenji Yuri Nakano <kenji.y.nakano@gmail.com>
2025-03-12 10:50:42 +0100

    Implemented AND operation for bitmap

--- src/xorec/xorec_utils.h
+++ src/xorec/xorec_utils.h
@@ -8,19 +8,19 @@
 #include <bitset>
 #include <cstdint>
 
-#define XOR_RESTRICT __restrict
+#define XOREC_RESTRICT __restrict
 
-constexpr uint32_t XOR_BLOCK_SIZE_MULTIPLE = 64;
-constexpr uint32_t XOR_MIN_BLOCK_SIZE = 64;
+constexpr uint32_t XOREC_BLOCK_SIZE_MULTIPLE = 64;
+constexpr uint32_t XOREC_MIN_BLOCK_SIZE = 64;
 
-constexpr uint32_t XOR_MIN_DATA_BLOCKS = 1;
-constexpr uint32_t XOR_MAX_DATA_BLOCKS = 128;
-constexpr uint32_t XOR_MIN_PARITY_BLOCKS = 1;
-constexpr uint32_t XOR_MAX_PARITY_BLOCKS = 128;
-constexpr uint32_t XOR_MAX_TOTAL_BLOCKS = 256;
+constexpr uint32_t XOREC_MIN_DATA_BLOCKS = 1;
+constexpr uint32_t XOREC_MAX_DATA_BLOCKS = 128;
+constexpr uint32_t XOREC_MIN_PARITY_BLOCKS = 1;
+constexpr uint32_t XOREC_MAX_PARITY_BLOCKS = 128;
+constexpr uint32_t XOREC_MAX_TOTAL_BLOCKS = 256;
 
 /// Bitmap to check if all data blocks are available (no recovery needed)
-const std::bitset<XOR_MAX_TOTAL_BLOCKS> COMPLETE_DATA_BITMAP =(
+const std::bitset<XOREC_MAX_TOTAL_BLOCKS> COMPLETE_DATA_BITMAP =(
   std::bitset<256>(0xFFFFFFFFFFFFFFFFULL)<<64) |
   std::bitset<256>(0xFFFFFFFFFFFFFFFFULL
   );
@@ -29,7 +29,7 @@ const std::bitset<XOR_MAX_TOTAL_BLOCKS> COMPLETE_DATA_BITMAP =(
  * @enum XORResult
  * @brief Represents the result status of encoding and decoding operations.
  */
-enum class XORResult {
+enum class XorecResult {
   Success = 0,
   InvalidSize = 1,
   InvalidCounts = 2,
@@ -42,7 +42,7 @@ enum class XORResult {
  * @enum XORVersion
  * @brief Allows to specify which version of the implementations to use
  */
-enum class XORVersion {
+enum class XorecVersion {
   Auto = 0,
   Scalar = 1,
   AVX = 2,
@@ -60,23 +60,39 @@ extern bool XOREC_GPU_INIT_CALLED;
 
 void xorec_gpu_init();
 
-static XORResult inline xorec_check_args(uint32_t block_size, uint32_t num_data_blocks, uint32_t num_parity_blocks) {
-  if (block_size < XOR_MIN_BLOCK_SIZE || block_size % XOR_BLOCK_SIZE_MULTIPLE != 0) {
-    return XORResult::InvalidSize;
+static XorecResult inline xorec_check_args(uint32_t block_size, uint32_t num_data_blocks, uint32_t num_parity_blocks) {
+  if (block_size < XOREC_MIN_BLOCK_SIZE || block_size % XOREC_BLOCK_SIZE_MULTIPLE != 0) {
+    return XorecResult::InvalidSize;
   }
 
   if (
-    num_data_blocks + num_parity_blocks > XOR_MAX_TOTAL_BLOCKS ||
-    num_data_blocks < XOR_MIN_DATA_BLOCKS ||
-    num_data_blocks > XOR_MAX_DATA_BLOCKS ||
-    num_parity_blocks < XOR_MIN_PARITY_BLOCKS ||
-    num_parity_blocks > XOR_MAX_PARITY_BLOCKS ||
+    num_data_blocks + num_parity_blocks > XOREC_MAX_TOTAL_BLOCKS ||
+    num_data_blocks < XOREC_MIN_DATA_BLOCKS ||
+    num_data_blocks > XOREC_MAX_DATA_BLOCKS ||
+    num_parity_blocks < XOREC_MIN_PARITY_BLOCKS ||
+    num_parity_blocks > XOREC_MAX_PARITY_BLOCKS ||
     num_parity_blocks > num_data_blocks ||
     num_data_blocks % num_parity_blocks != 0
   ) {
-        return XORResult::InvalidCounts;
+        return XorecResult::InvalidCounts;
   }
-  return XORResult::Success;
+  return XorecResult::Success;
+}
+
+static void inline and_bitmap(uint8_t * dst, const uint8_t *src1, const uint8_t *src2, uint32_t count) {
+  uint64_t * dst_64 = reinterpret_cast<uint64_t*>(dst);
+  const uint64_t * src1_64 = reinterpret_cast<const uint64_t*>(src1);
+  const uint64_t * src2_64 = reinterpret_cast<const uint64_t*>(src2);
+  uint32_t i;
+
+  for (i = 0; i < count/4; ++i) {
+    dst_64[i] = src1_64[i] & src2_64[i];
+  }
+
+  i *= 4;
+  for (; i < count; ++i) {
+    dst[i] = src1[i] & src2[i];
+  }
 }
 
 
