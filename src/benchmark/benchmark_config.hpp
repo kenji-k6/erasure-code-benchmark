74c614922c89c89a07ea83814eb2e16dc154b95a
Kenji Yuri Nakano <kenji.y.nakano@gmail.com>
2025-03-15 15:24:13 +0100

    Adjusted benchmark config values again

--- src/benchmark/benchmark_config.hpp
+++ src/benchmark/benchmark_config.hpp
@@ -53,6 +53,6 @@ constexpr uint64_t FIXED_NUM_LOST_BLOCKS = 1;
 const std::vector<uint64_t> VAR_BUFFER_SIZE = { 134217728, 67108864, 33554432, 16777216, 8388608, 4194304, 2097152, 1048576, 524288, 262144 };
 const std::vector<uint32_t> VAR_NUM_RECOVERY_BLOCKS = { 128, 64, 32, 16, 8, 4, 2, 1 };
 const std::vector<uint32_t> VAR_NUM_LOST_BLOCKS = { 128, 64, 32, 16, 8, 4, 2, 1 };
-const std::vector<uint32_t> VAR_NUM_PREFETCH_BYTES = { 65536, 131072, 262144, 524288, 1048576, 2097152,};
+const std::vector<uint32_t> VAR_NUM_PREFETCH_BYTES = { 262144, 524288, 1048576, 2097152,};
 
 #endif // BENCHMARK_CONFIG_HPP
