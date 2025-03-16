bc0a0160fb50d53bb1ca18e16897713b82dc1289
Kenji Yuri Nakano <kenji.y.nakano@gmail.com>
2025-03-16 13:14:15 +0100

    Implemented CPU info retrieval in hardware_info.py

--- scripts/utils/config.py
+++ scripts/utils/config.py
@@ -1,12 +1,11 @@
 import os
 
-# Input/Output directories and file constants
 RESULTS_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))), 'results')
 RAW_DIR = os.path.join(RESULTS_DIR, 'raw')
 PLOT_DIR = os.path.join(RESULTS_DIR, 'plots')
 INPUT_FILE = None
 
-# AVX(2) related constants
+
 AVX_XOR_CPI_XEON = 0.33
 AVX_COLOR = "cornflowerblue"
 AVX_BITS = 128
---
+++ scripts/utils/hardware_info.py
@@ -0,0 +1,31 @@
+import cpuinfo
+import psutil
+from collections import namedtuple
+
+CPUInfo = namedtuple("CPUInfo", [
+  "model_name",
+  "clock_rate_GHz",
+  "l1_cache_size_KiB",
+  "l2_cache_size_KiB",
+  "l3_cache_size_KiB"
+])
+
+
+def get_cpu_info() -> CPUInfo:
+  """Returns a namedtuple with information about the CPU."""
+  info = cpuinfo.get_cpu_info()
+  model_name = info["brand_raw"]
+
+  clock_rate_GHz = float(psutil.cpu_freq().max if psutil.cpu_freq().max else 0) / 1e3
+
+  l1_cache_size_KiB = int(info.get("l1_data_cache_size", 0)) // 1024
+  l2_cache_size_KiB = int(info.get("l2_cache_size", 0)) // 1024
+  l3_cache_size_KiB = int(info.get("l3_cache_size", 0)) // 1024
+
+  return CPUInfo(
+    model_name=model_name,
+    clock_rate_GHz=clock_rate_GHz,
+    l1_cache_size_KiB=l1_cache_size_KiB,
+    l2_cache_size_KiB=l2_cache_size_KiB,
+    l3_cache_size_KiB=l3_cache_size_KiB
+  )
