18a42c262242f23a0c017e30580a5514032394b2
Kenji Yuri Nakano <kenji.y.nakano@gmail.com>
2025-03-20 16:27:23 +0100

    Rewrote theoretical_bounds.py to work with new CSV data

--- scripts/utils/theoretical_bounds.py
+++ scripts/utils/theoretical_bounds.py
@@ -1,10 +1,12 @@
-from utils.utils import AxType
+from utils.utils import AxType, XorecVersion
 from typing import Callable
+import pandas as pd
+
+
+
 
 def get_theoretical_xor_encode_time(
-  simd_xor_latency: int,
-  simd_xor_throughput: float,
-  simd_bits: int,
+  perf_df: pd.DataFrame,
   clk_rate_GHz: float,
   blk_size_B: int,
   num_data_blks: int,
@@ -12,17 +14,19 @@ def get_theoretical_xor_encode_time(
   num_lost_data_blks: int
 ) -> float:
   """Calculate the theoretical minimum encoding time in ms (single threaded)"""
-  blk_size_bits = blk_size_B * 8
-  num_xors_per_block = blk_size_bits / simd_bits
-  num_cycles_per_block = simd_xor_latency + (num_xors_per_block - 1) * simd_xor_throughput
-  block_xor_time_ms = (num_cycles_per_block / clk_rate_GHz) / 1e6
-  return block_xor_time_ms * num_data_blks
+  # Sanity check
+  assert(len(perf_df.loc[perf_df["block_size_B"] == blk_size_B, "num_cycles"].values) == 1)
+
+  num_cycles = perf_df.loc[perf_df["block_size_B"] == blk_size_B, "num_cycles"].values[0]
+  block_xor_time_ms = (num_cycles / clk_rate_GHz) / 1e6
+
+  num_calls = num_data_blks # number of calls to xorec_xor_blocks_<version>
+  
+  return block_xor_time_ms * num_calls
 
 
 def get_theoretical_xor_encode_throughput(
-  simd_xor_latency: int,
-  simd_xor_throughput: float,
-  simd_bits: int,
+  perf_df: pd.DataFrame,
   clk_rate_GHz: float,
   blk_size_B: int,
   num_data_blks: int,
@@ -31,9 +35,7 @@ def get_theoretical_xor_encode_throughput(
 ) -> float:
   """Calculate the theoretical maximum encoding throughput in Gbps (single threaded)"""
   encode_time_ms = get_theoretical_xor_encode_time(
-    simd_xor_latency,
-    simd_xor_throughput,
-    simd_bits,
+    perf_df,
     clk_rate_GHz,
     blk_size_B,
     num_data_blks,
@@ -46,9 +48,7 @@ def get_theoretical_xor_encode_throughput(
 
 
 def get_theoretical_xor_decode_time(
-  simd_xor_latency: int,
-  simd_xor_throughput: float,
-  simd_bits: int,
+  perf_df: pd.DataFrame,
   clk_rate_GHz: float,
   blk_size_B: int,
   num_data_blks: int,
@@ -56,21 +56,21 @@ def get_theoretical_xor_decode_time(
   num_lost_data_blks: int
 ) -> float:
   """Calculate the theoretical minimum decoding time in ms (single threaded)"""
-  blk_size_bits = blk_size_B * 8
-  num_xors_per_block = blk_size_bits / simd_bits
-  num_cycles_per_block = simd_xor_latency + (num_xors_per_block - 1) * simd_xor_throughput
-  block_xor_time_ms = (num_cycles_per_block / clk_rate_GHz) / 1e6
+  # Sanity check
+  assert(len(perf_df.loc[perf_df["block_size_B"] == blk_size_B, "num_cycles"].values) == 1)
+  
+  num_cycles = perf_df.loc[perf_df["block_size_B"] == blk_size_B, "num_cycles"].values[0]
+  block_xor_time_ms = (num_cycles / clk_rate_GHz) / 1e6
 
-  blks_per_par_blk = num_data_blks / num_parity_blks
+  xors_per_par_blk = num_data_blks/num_parity_blks # number of XORs per parity block
 
-  time_per_lst_blk = block_xor_time_ms * blks_per_par_blk
-  return time_per_lst_blk * num_lost_data_blks
+  num_calls = num_lost_data_blks * xors_per_par_blk # number of calls to xorec_xor_blocks_<version>
+  
+  return block_xor_time_ms * num_calls
 
 
 def get_theoretical_xor_decode_throughput(
-  simd_xor_latency: int,
-  simd_xor_throughput: float,
-  simd_bits: int,
+  perf_df: pd.DataFrame,
   clk_rate_GHz: float,
   blk_size_B: int,
   num_data_blks: int,
@@ -79,9 +79,7 @@ def get_theoretical_xor_decode_throughput(
 ) -> float:
   """Calculate the theoretical maximum decoding throughput in Gbps (single threaded)"""
   decode_time_ms = get_theoretical_xor_decode_time(
-    simd_xor_latency,
-    simd_xor_throughput,
-    simd_bits,
+    perf_df,
     clk_rate_GHz,
     blk_size_B,
     num_data_blks,
@@ -93,9 +91,7 @@ def get_theoretical_xor_decode_throughput(
   return decode_throughput_Gbps
 
 
-
-
-def get_theoretical_bound_func(ax: AxType) -> Callable[[int, float, int, float, int, int, int, int], float]:
+def get_theoretical_bound_func(ax: AxType) -> Callable[[pd.DataFrame, float, int, int, int, int], float]:
   if ax == AxType.ENCODE_T: return get_theoretical_xor_encode_time
   if ax == AxType.ENCODE_TP: return get_theoretical_xor_encode_throughput
   if ax == AxType.DECODE_T: return get_theoretical_xor_decode_time
