c895de612ce454e698ae16b76ee29888c7a3bfcb
Kenji Yuri Nakano <kenji.y.nakano@gmail.com>
2025-03-20 17:22:12 +0100

    Removed #gcc pragma ivdep

--- src/xorec/xorec.hpp
+++ src/xorec/xorec.hpp
@@ -176,7 +176,6 @@ static void inline xorec_xor_blocks_avx512(void * XOREC_RESTRICT dest, const voi
     XOREC_AVX512 * XOREC_RESTRICT dest512 = reinterpret_cast<XOREC_AVX512*>(dest);
     const XOREC_AVX512 * XOREC_RESTRICT src512 = reinterpret_cast<const XOREC_AVX512*>(src);
 
-    #pragma GCC ivdep
     for (; bytes >= 256; bytes -= 256, dest512 += 4, src512 += 4) {
       XOREC_AVX512 x0 = _mm512_xor_si512(_mm512_loadu_si512(dest512), _mm512_loadu_si512(src512));
       XOREC_AVX512 x1 = _mm512_xor_si512(_mm512_loadu_si512(dest512 + 1), _mm512_loadu_si512(src512 + 1));
@@ -220,7 +219,6 @@ static void inline xorec_xor_blocks_avx2(void * XOREC_RESTRICT dest, const void 
     XOREC_AVX2 * XOREC_RESTRICT dest256 = reinterpret_cast<XOREC_AVX2*>(dest);
     const XOREC_AVX2 * XOREC_RESTRICT src256 = reinterpret_cast<const XOREC_AVX2*>(src);
 
-    #pragma GCC ivdep
     for (; bytes >= 128; bytes -= 128, dest256 += 4, src256 += 4) {
       XOREC_AVX2 x0 = _mm256_xor_si256(_mm256_loadu_si256(dest256), _mm256_loadu_si256(src256));
       XOREC_AVX2 x1 = _mm256_xor_si256(_mm256_loadu_si256(dest256 + 1), _mm256_loadu_si256(src256 + 1));
@@ -256,7 +254,6 @@ static void inline xorec_xor_blocks_avx(void * XOREC_RESTRICT dest, const void *
     XOREC_AVX * XOREC_RESTRICT dest128 = reinterpret_cast<XOREC_AVX*>(dest);
     const XOREC_AVX * XOREC_RESTRICT src128 = reinterpret_cast<const XOREC_AVX*>(src);
 
-    #pragma GCC ivdep
     for (; bytes >= 64; bytes -= 64, dest128 += 4, src128 += 4) {
       XOREC_AVX x0 = _mm_xor_si128(_mm_loadu_si128(dest128), _mm_loadu_si128(src128));
       XOREC_AVX x1 = _mm_xor_si128(_mm_loadu_si128(dest128 + 1), _mm_loadu_si128(src128 + 1));
