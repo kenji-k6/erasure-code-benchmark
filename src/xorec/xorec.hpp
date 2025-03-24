09d30da083ab1b9256753886c2336792d8627259
Kenji Yuri Nakano <kenji.y.nakano@gmail.com>
2025-03-24 11:11:43 +0100

    Fixed error with __asume_aligned (changed to __builtin_assume_aligned)

--- src/xorec/xorec.hpp
+++ src/xorec/xorec.hpp
@@ -173,11 +173,9 @@ XorecResult xorec_gpu_prefetch_decode(
  */
 static void inline xorec_xor_blocks_avx512(void * XOREC_RESTRICT dest, const void * XOREC_RESTRICT src, size_t bytes) {
   #if defined(TRY_XOREC_AVX512)
-    XOREC_AVX512 * XOREC_RESTRICT dest512 = reinterpret_cast<XOREC_AVX512*>(dest);
-    const XOREC_AVX512 * XOREC_RESTRICT src512 = reinterpret_cast<const XOREC_AVX512*>(src);
+    XOREC_AVX512 * XOREC_RESTRICT dest512 = reinterpret_cast<XOREC_AVX512*>(__builtin_assume_aligned(dest, 64));
+    const XOREC_AVX512 * XOREC_RESTRICT src512 = reinterpret_cast<const XOREC_AVX512*>(__builtin_assume_aligned(src, 64));
 
-    __assume_aligned(dest512, 64);
-    __assume_aligned(src512, 64);
     for (; bytes >= 256; bytes -= 256, dest512 += 4, src512 += 4) {
       XOREC_AVX512 x0 = _mm512_xor_si512(_mm512_load_si512(dest512), _mm512_load_si512(src512));
       XOREC_AVX512 x1 = _mm512_xor_si512(_mm512_load_si512(dest512 + 1), _mm512_load_si512(src512 + 1));
@@ -189,8 +187,6 @@ static void inline xorec_xor_blocks_avx512(void * XOREC_RESTRICT dest, const voi
       _mm512_store_si512(dest512 + 3, x3);
     }
 
-    __assume_aligned(dest512, 64);
-    __assume_aligned(src512, 64);
     if (bytes > 64) {
       XOREC_AVX512 x0 = _mm512_xor_si512(_mm512_load_si512(dest512), _mm512_load_si512(src512));
       XOREC_AVX512 x1 = _mm512_xor_si512(_mm512_load_si512(dest512 + 1), _mm512_load_si512(src512 + 1));
@@ -200,8 +196,6 @@ static void inline xorec_xor_blocks_avx512(void * XOREC_RESTRICT dest, const voi
       src512 += 2;
     }
 
-    __assume_aligned(dest512, 64);
-    __assume_aligned(src512, 64);
     if (bytes > 0) {
       XOREC_AVX512 x0 = _mm512_xor_si512(_mm512_load_si512(dest512), _mm512_load_si512(src512));
       _mm512_store_si512(dest512, x0);
@@ -222,11 +216,9 @@ static void inline xorec_xor_blocks_avx512(void * XOREC_RESTRICT dest, const voi
  */
 static void inline xorec_xor_blocks_avx2(void * XOREC_RESTRICT dest, const void * XOREC_RESTRICT src, size_t bytes) {
   #if defined(TRY_XOREC_AVX2)
-    XOREC_AVX2 * XOREC_RESTRICT dest256 = reinterpret_cast<XOREC_AVX2*>(dest);
-    const XOREC_AVX2 * XOREC_RESTRICT src256 = reinterpret_cast<const XOREC_AVX2*>(src);
+    XOREC_AVX2 * XOREC_RESTRICT dest256 = reinterpret_cast<XOREC_AVX2*>(__builtin_assume_aligned(dest, 64));
+    const XOREC_AVX2 * XOREC_RESTRICT src256 = reinterpret_cast<const XOREC_AVX2*>(__builtin_assume_aligned(src, 64));
 
-    __assume_aligned(dest256, 64);
-    __assume_aligned(src256, 64);
     for (; bytes >= 128; bytes -= 128, dest256 += 4, src256 += 4) {
       XOREC_AVX2 x0 = _mm256_xor_si256(_mm256_load_si256(dest256), _mm256_load_si256(src256));
       XOREC_AVX2 x1 = _mm256_xor_si256(_mm256_load_si256(dest256 + 1), _mm256_load_si256(src256 + 1));
@@ -238,8 +230,6 @@ static void inline xorec_xor_blocks_avx2(void * XOREC_RESTRICT dest, const void 
       _mm256_store_si256(dest256 + 3, x3);
     }
 
-    __assume_aligned(dest256, 64);
-    __assume_aligned(src256, 64);
     if (bytes > 0) {
       XOREC_AVX2 x0 = _mm256_xor_si256(_mm256_load_si256(dest256), _mm256_load_si256(src256));
       XOREC_AVX2 x1 = _mm256_xor_si256(_mm256_load_si256(dest256 + 1), _mm256_load_si256(src256 + 1));
@@ -261,11 +251,9 @@ static void inline xorec_xor_blocks_avx2(void * XOREC_RESTRICT dest, const void 
  */
 static void inline xorec_xor_blocks_avx(void * XOREC_RESTRICT dest, const void * XOREC_RESTRICT src, size_t bytes) {
   #if defined(TRY_XOREC_AVX)
-    XOREC_AVX * XOREC_RESTRICT dest128 = reinterpret_cast<XOREC_AVX*>(dest);
-    const XOREC_AVX * XOREC_RESTRICT src128 = reinterpret_cast<const XOREC_AVX*>(src);
+    XOREC_AVX * XOREC_RESTRICT dest128 = reinterpret_cast<XOREC_AVX*>(__builtin_assume_aligned(dest, 64));
+    const XOREC_AVX * XOREC_RESTRICT src128 = reinterpret_cast<const XOREC_AVX*>(__builtin_assume_aligned(src, 64));
 
-    __assume_aligned(dest128, 64);
-    __assume_aligned(src128, 64);
     for (; bytes >= 64; bytes -= 64, dest128 += 4, src128 += 4) {
       XOREC_AVX x0 = _mm_xor_si128(_mm_load_si128(dest128), _mm_load_si128(src128));
       XOREC_AVX x1 = _mm_xor_si128(_mm_load_si128(dest128 + 1), _mm_load_si128(src128 + 1));
