21338ce756adfdefee5e861a3142060824e72635
Kenji Yuri Nakano <kenji.y.nakano@gmail.com>
2025-03-26 15:35:47 +0100

    Fixed bugs in cm256_bm.cpp

--- src/algorithms/cm256_bm.cpp
+++ src/algorithms/cm256_bm.cpp
@@ -11,13 +11,11 @@
 
 CM256Benchmark::CM256Benchmark(const BenchmarkConfig& config) noexcept : ECBenchmark(config) {
   if (cm256_init()) throw_error("CM256: Initialization failed.");
-  size_t num_data_blocks = get<0>(m_fec_params);
-  size_t num_parity_blocks = get<1>(m_fec_params);
 
   // Initialize CM256 parameters
   m_params.BlockBytes = m_size_blk;
-  m_params.OriginalCount = num_data_blocks;
-  m_params.RecoveryCount = num_parity_blocks;
+  m_params.OriginalCount = m_data_blks_per_chunk;
+  m_params.RecoveryCount = m_parity_blks_per_chunk;
 
   // Allocate memory
   m_data_buffer = reinterpret_cast<uint8_t*>(_mm_malloc(m_num_chunks * m_size_data_submsg, ALIGNMENT));
@@ -31,17 +29,21 @@ CM256Benchmark::CM256Benchmark(const BenchmarkConfig& config) noexcept : ECBench
     if (write_validation_pattern(i, m_data_buffer+i*m_size_blk, m_size_blk)) throw_error("CM256: Failed to write validation pattern");
   }
 
+
+  m_blocks.resize(ECLimits::CM256_MAX_TOT_BLOCKS*m_num_chunks);
+
   // Initialize indices
   for (unsigned i = 0; i < m_num_chunks; ++i) {
-    for (unsigned idx = 0; idx < num_data_blocks; ++i) {
-      m_blocks[i*num_data_blocks + idx].Index = cm256_get_original_block_index(m_params, idx);
+    for (unsigned idx = 0; idx < m_data_blks_per_chunk; ++idx) {
+      m_blocks[i*ECLimits::CM256_MAX_TOT_BLOCKS + idx].Index = cm256_get_original_block_index(m_params, idx);
     }
   }
-  
+
   // Initialize block vector
-  m_blocks.resize(ECLimits::CM256_MAX_TOT_BLOCKS*m_num_chunks);
-  for (unsigned i = 0; i < m_size_msg/m_size_blk; ++i) {
-    m_blocks[i].Block = m_data_buffer + i * m_size_blk;
+  for (unsigned i = 0; i < m_num_chunks; ++i) {
+    for (unsigned idx = 0; idx < m_data_blks_per_chunk; ++idx) {
+      m_blocks[i*ECLimits::CM256_MAX_TOT_BLOCKS + idx].Block = m_data_buffer + i * m_size_data_submsg + idx * m_size_blk;
+    }
   }
 
 }
@@ -53,19 +55,28 @@ CM256Benchmark::~CM256Benchmark() noexcept {
 }
 
 int CM256Benchmark::encode() noexcept {
+  cm256_block* blocks = m_blocks.data();
+  uint8_t* parity_ptr = m_parity_buffer;
+
   for (unsigned i = 0; i < m_num_chunks; ++i) {
-    uint8_t* parity_ptr = m_parity_buffer + i * m_size_parity_submsg;
-    cm256_block* blocks = m_blocks.data() + i * ECLimits::CM256_MAX_TOT_BLOCKS;
     if (cm256_encode(m_params, blocks, parity_ptr)) return 1;
+    
+    blocks += ECLimits::CM256_MAX_TOT_BLOCKS;
+    parity_ptr += m_size_parity_submsg;
   }
   return 0;
 }
 
 int CM256Benchmark::decode() noexcept {
+  cm256_block* blocks = m_blocks.data();
+  uint8_t* bitmap = m_block_bitmap;
+
   for (unsigned i = 0; i < m_num_chunks; ++i) {
-    cm256_block* blocks = m_blocks.data() + i * ECLimits::CM256_MAX_TOT_BLOCKS;
     if (cm256_decode(m_params, blocks)) return 1;
+    blocks += ECLimits::CM256_MAX_TOT_BLOCKS;
+    bitmap += m_blks_per_chunk;
   }
+
   return 0;
 }
 
