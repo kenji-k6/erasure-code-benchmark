1f1659f7e7638ef9ce15a388aabfe17ae497e890
Kenji Yuri Nakano <kenji.y.nakano@gmail.com>
2025-04-01 16:39:45 +0200

    Updated main.cpp & CMakeLists.txt

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,6 @@
 #include "bm_utils.hpp"
 
 int main (int argc, char** argv) {
-  std::vector<BenchmarkConfig> ec_configs;
-  std::vector<BenchmarkConfig> perf_configs;
-  std::vector<std::vector<uint32_t>> lost_block_idxs;
-  
-  parse_args(argc, argv);
-  get_configs(ec_configs, lost_block_idxs, perf_configs);
-  run_benchmarks(ec_configs, perf_configs);
+  run_benchmarks(argc, argv);
+  return 0;
 }
