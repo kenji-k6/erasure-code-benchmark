f5f796b1749b4828f69b2ecf032e81e9d2adce83
Kenji Yuri Nakano <kenji.y.nakano@gmail.com>
2025-05-03 17:37:01 +0200

    Adjusted Xorec GPU implementation

--- src/xorec/xorec_gpu_cmp.cu
+++ src/xorec/xorec_gpu_cmp.cu
@@ -28,76 +28,113 @@ void xorec_gpu_init(size_t num_data_blocks, int device_id) {
 XorecResult xorec_gpu_encode(
   const uint8_t *XOREC_RESTRICT data_buf,
   uint8_t *XOREC_RESTRICT parity_buf,
+  size_t num_chunks,
   size_t block_size,
-  size_t num_data_blocks,
-  size_t num_parity_blocks,
+  size_t chunk_data_blocks,
+  size_t chunk_parity_blocks,
   size_t num_gpu_blocks,
   size_t threads_per_block
 ) {
   if (!XOREC_GPU_INIT_CALLED) throw_error("xorec_gpu_init() must be called before calling xorec_encode()");
-  XorecResult err = xorec_check_args(data_buf, parity_buf, block_size, num_data_blocks, num_parity_blocks);
+  XorecResult err = xorec_check_args(data_buf, parity_buf, block_size, chunk_data_blocks, chunk_parity_blocks);
   if (err != XorecResult::Success) return err;
   if (block_size % sizeof(CUDA_ATOMIC_XOR_T) != 0) return XorecResult::InvalidSize;
 
-  cudaMemsetAsync(parity_buf, 0, block_size * num_parity_blocks);
-  xorec_gpu_xor_parity_kernel<<<num_gpu_blocks, threads_per_block>>>(data_buf, parity_buf, block_size, num_data_blocks, num_parity_blocks);
+  cudaMemsetAsync(parity_buf, 0, num_chunks * chunk_parity_blocks * block_size);
+  xorec_gpu_xor_kernel<<<num_gpu_blocks, threads_per_block>>>(
+    data_buf,
+    parity_buf,
+    num_chunks,
+    block_size,
+    chunk_data_blocks,
+    chunk_parity_blocks
+  );
+  
   return XorecResult::Success;
 }
 
 
 XorecResult xorec_gpu_decode(
-  uint8_t *XOREC_RESTRICT data_buf,
-  uint8_t *XOREC_RESTRICT parity_buf,
+  uint8_t* XOREC_RESTRICT data_buf,
+  uint8_t* XOREC_RESTRICT parity_buf,
+  size_t num_chunks,
   size_t block_size,
-  size_t num_data_blocks,
-  size_t num_parity_blocks,
-  const uint8_t *XOREC_RESTRICT block_bitmap,   ///< Indexing for parity blocks starts at bit 128, e.g. the j-th parity block is at bit 128 + j, j < 128
+  size_t chunk_data_blocks,
+  size_t chunk_parity_blocks,
+  const uint8_t* XOREC_RESTRICT block_bitmap,
   size_t num_gpu_blocks,
   size_t threads_per_block
 ) {
   if (!XOREC_GPU_INIT_CALLED) throw_error("xorec_gpu_init() must be called before calling xorec_encode()");
 
-  XorecResult err = xorec_check_args(data_buf, parity_buf, block_size, num_data_blocks, num_parity_blocks);
+  XorecResult err = xorec_check_args(data_buf, parity_buf, block_size, chunk_data_blocks, chunk_parity_blocks);
   if (err != XorecResult::Success) return err;
   if (block_size % sizeof(CUDA_ATOMIC_XOR_T) != 0) return XorecResult::InvalidSize;
 
-  if (!require_recovery(num_data_blocks, block_bitmap)) return XorecResult::Success;
-  if (!is_recoverable(num_data_blocks, num_parity_blocks, block_bitmap)) return XorecResult::DecodeFailure;
+  for (unsigned c = 0; c < num_chunks; ++c) {
+    auto chunk_bitmap = block_bitmap + c * (chunk_data_blocks + chunk_parity_blocks);
+    auto chunk_data_buf = data_buf + c * chunk_data_blocks * block_size;
+    if (!require_recovery(chunk_data_blocks, chunk_bitmap)) return XorecResult::Success;
+    if (!is_recoverable(chunk_data_blocks, chunk_parity_blocks, chunk_bitmap)) return XorecResult::DecodeFailure;
 
-  
-  // Zero out lost blocks
-  for (uint32_t i = 0; i < num_data_blocks; ++i) {
-    if (!block_bitmap[i]) cudaMemsetAsync(data_buf + i * block_size, 0, block_size);
+    // Zero out lost blocks
+    for (uint32_t i = 0; i < chunk_data_blocks; ++i) {
+      if (!chunk_bitmap[i]) cudaMemsetAsync(chunk_data_buf + i * block_size, 0, block_size);
+    }
   }
 
-  xorec_gpu_xor_parity_kernel<<<num_gpu_blocks, threads_per_block>>>(data_buf, parity_buf, block_size, num_data_blocks, num_parity_blocks);
+  xorec_gpu_xor_kernel<<<num_gpu_blocks, threads_per_block>>>(
+    data_buf,
+    parity_buf,
+    num_chunks,
+    block_size,
+    chunk_data_blocks,
+    chunk_parity_blocks
+  );
 
-  for (uint32_t i = 0; i < num_data_blocks; ++i) {
-    if (!block_bitmap[i]) cudaMemcpyAsync(data_buf + i * block_size, parity_buf + (i % num_parity_blocks) * block_size, block_size, cudaMemcpyDeviceToDevice);
+  // Copy back the lost blocks
+  for (unsigned c = 0; c < num_chunks; ++c) {
+    auto chunk_bitmap = block_bitmap + c * (chunk_data_blocks + chunk_parity_blocks);
+    auto chunk_data_buf = data_buf + c * chunk_data_blocks * block_size;
+
+    for (unsigned i = 0; i < chunk_data_blocks; ++i) {
+      if (!chunk_bitmap[i]) cudaMemcpyAsync(chunk_data_buf + i * block_size, parity_buf + (i % chunk_parity_blocks) * block_size, block_size, cudaMemcpyDeviceToDevice);
+    }
   }
   return XorecResult::Success;
 }
 
-__global__ void xorec_gpu_xor_parity_kernel(
-  const uint8_t * XOREC_RESTRICT data_buf,
-  uint8_t * XOREC_RESTRICT parity_buf,
+
+
+__global__ void xorec_gpu_xor_kernel(
+  const uint8_t* XOREC_RESTRICT data_buf,
+  uint8_t* XOREC_RESTRICT parity_buf,
+  size_t num_chunks,
   size_t block_size,
-  size_t num_data_blocks,
-  size_t num_parity_blocks
+  size_t chunk_data_blocks,
+  size_t chunk_parity_blocks
 ) {
   unsigned num_threads = blockDim.x * gridDim.x;
   unsigned glbl_thread_idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+  unsigned tot_elems = num_chunks * chunk_data_blocks * block_size / sizeof(CUDA_ATOMIC_XOR_T);
+  unsigned chunk_elems = chunk_data_blocks * block_size / sizeof(CUDA_ATOMIC_XOR_T);
   unsigned block_elems = block_size / sizeof(CUDA_ATOMIC_XOR_T);
-  unsigned tot_elems = num_data_blocks * block_elems;
 
   for (unsigned i = glbl_thread_idx; i < tot_elems; i += num_threads) {
-    unsigned block_idx = i / block_elems;
-    unsigned parity_idx = block_idx % num_parity_blocks;
+    unsigned chunk_idx = i / chunk_elems;
+    unsigned block_idx = (i % chunk_elems) / block_elems;
+    unsigned parity_idx = block_idx % chunk_parity_blocks;
 
-    const CUDA_ATOMIC_XOR_T * XOREC_RESTRICT data_block_64 = reinterpret_cast<const CUDA_ATOMIC_XOR_T*>(data_buf + block_idx * block_size);
-    CUDA_ATOMIC_XOR_T * XOREC_RESTRICT parity_block_64 = reinterpret_cast<CUDA_ATOMIC_XOR_T*>(parity_buf + parity_idx * block_size);
+    const CUDA_ATOMIC_XOR_T * XOREC_RESTRICT data_block = reinterpret_cast<const CUDA_ATOMIC_XOR_T*>(
+      data_buf + (chunk_idx * chunk_data_blocks * block_size) + (block_idx * block_size)
+    );
+    CUDA_ATOMIC_XOR_T * XOREC_RESTRICT parity_block = reinterpret_cast<CUDA_ATOMIC_XOR_T*>(
+      parity_buf + (chunk_idx * chunk_parity_blocks * block_size) + (parity_idx * block_size)
+    );
 
-    atomicXor(&parity_block_64[i % block_elems], data_block_64[i % block_elems]);
+    atomicXor(&parity_block[(i % chunk_elems)%block_elems], data_block[(i% chunk_elems)%block_elems]);
   }
 }
+
 
--- src/xorec/xorec_gpu_cmp.cuh
+++ src/xorec/xorec_gpu_cmp.cuh
@@ -18,9 +18,10 @@ void xorec_gpu_init(size_t num_data_blocks, int device_id=0);
  * 
  * @param data_buf The data buffer to encode. Must be allocated in unified memory.
  * @param parity_buf The parity buffer to write the encoded parity blocks to. Must be allocated in unified memory.
+ * @param num_chunks Number of chunks to encode.
  * @param block_size Size of each block in bytes.
- * @param num_data_blocks Number of data blocks.
- * @param num_parity_blocks Number of parity blocks.
+ * @param chunk_data_blocks Number of data blocks per chunk.
+ * @param chunk_parity_blocks Number of parity blocks per chunk.
  * @param num_gpu_blocks Number of GPU blocks to use for encoding.
  * @param threads_per_block Number of threads per block to use for encoding.
  * @return XorecResult 
@@ -28,9 +29,10 @@ void xorec_gpu_init(size_t num_data_blocks, int device_id=0);
 XorecResult xorec_gpu_encode(
   const uint8_t *XOREC_RESTRICT data_buf,
   uint8_t *XOREC_RESTRICT parity_buf,
+  size_t num_chunks,
   size_t block_size,
-  size_t num_data_blocks,
-  size_t num_parity_blocks,
+  size_t chunk_data_blocks,
+  size_t chunk_parity_blocks,
   size_t num_gpu_blocks,
   size_t threads_per_block
 );
@@ -51,22 +53,36 @@ XorecResult xorec_gpu_encode(
 XorecResult xorec_gpu_decode(
   uint8_t* XOREC_RESTRICT data_buf,
   uint8_t* XOREC_RESTRICT parity_buf,
+  size_t num_chunks,
   size_t block_size,
-  size_t num_data_blocks,
-  size_t num_parity_blocks,
+  size_t chunk_data_blocks,
+  size_t chunk_parity_blocks,
   const uint8_t* XOREC_RESTRICT block_bitmap,
   size_t num_gpu_blocks,
   size_t threads_per_block
 );
 
-__global__ void xorec_gpu_xor_parity_kernel(
+
+/**
+ * @brief XOR-EC GPU encoding kernel.
+ */
+__global__ void xorec_gpu_xor_kernel(
   const uint8_t* XOREC_RESTRICT data_buf,
   uint8_t* XOREC_RESTRICT parity_buf,
+  size_t num_chunks,
   size_t block_size,
-  size_t num_data_blocks,
-  size_t num_parity_blocks
+  size_t chunk_data_blocks,
+  size_t chunk_parity_blocks
 );
 
+__global__ void xorec_gpu_decode_kernel(
+  uint8_t* XOREC_RESTRICT data_chunk_buf,
+  const uint8_t* XOREC_RESTRICT parity_chunk_buf,
+  size_t lost_block_idx,
+  size_t block_size,
+  size_t chunk_data_blocks,
+  size_t chunk_parity_blocks
+);
 
 
 #endif // XOREC_GPU_CMP_CUH
