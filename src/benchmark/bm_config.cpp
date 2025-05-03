b53b19634328507b599e8b900a1cbce0be506a78
Kenji Yuri Nakano <kenji.y.nakano@gmail.com>
2025-05-03 15:19:20 +0200

    Adjusted bm_config.hpp/cpp for multi-threaded variant and new parameters

--- src/benchmark/bm_config.cpp
+++ src/benchmark/bm_config.cpp
@@ -1,25 +1,19 @@
 #include "bm_config.hpp"
-#define KiB *1024
-# define MiB *1024*1024
 
-const std::vector<size_t> VAR_DATA_SIZES = {
-  128 KiB, 256 KiB, 512 KiB, 1 MiB, 2 MiB
+const std::vector<size_t> VAR_BLOCK_SIZES = {
+  4 KiB, 8 KiB, 16 KiB, 32 KiB, 64 KiB
 };
 
 const std::vector<ECTuple> VAR_EC_PARAMS = {
-  { 4+32, 32 },     { 8+32, 32 },     { 16+32, 32 },
-  { 8+64, 64 },     { 16+64, 64 },    { 32+64, 64 },
-  { 16+128, 128 },  { 32+128, 128 },  { 64+128, 128 }
+  { 4+8,  8 },
+  { 4+16, 16 }, { 8+16, 16 },
+  { 4+32, 32 }, { 8+32, 32 },
+};
+
+const std::vector<size_t> VAR_NUM_CPU_THREADS = {
+  1, 2, 4, 8, 16, 32
 };
 
 const std::vector<size_t> VAR_NUM_LOST_BLOCKS = {
-  0, 1, 2, 4, 8, 16, 32, 64
-};
-
-const std::vector<size_t> VAR_NUM_GPU_BLOCKS = {
-  8, 16, 32, 64, 128, 256
-};
-
-const std::vector<size_t> VAR_NUM_THREADS_PER_BLOCK = {
-  32, 64, 128, 256, 512
+  0, 1, 2, 4, 8
 };
--- src/benchmark/bm_config.hpp
+++ src/benchmark/bm_config.hpp
@@ -6,6 +6,9 @@
 #ifndef BM_CONFIG_HPP
 #define BM_CONFIG_HPP
 
+#define KiB *1024
+#define MiB *1024*1024
+
 #include "console_reporter.hpp"
 #include "xorec_utils.hpp"
 #include <cstdint>
@@ -20,17 +23,19 @@ using ECTuple = std::tuple<size_t, size_t>;
  * @brief Struct to hold the configuration for a single benchmark run
  */
 struct BenchmarkConfig {
-  size_t data_size;                               ///< Total size of original data (in bytes)
+  size_t message_size;                            ///< Size of the original message to be encoded (in bytes)
   size_t block_size;                              ///< Size of each block (in bytes)
   ECTuple ec_params;                            
   size_t num_lost_blocks;                         ///< Number of total blocks lost (recovery + original)
 
+  size_t num_cpu_threads;                         ///< Number of CPU threads to use
+
   int num_iterations;                             ///< Number of iterations to run the benchmark
-  int num_warmup_iterations;                     ///< Number of warm-up iterations
+  int num_warmup_iterations;                      ///< Number of warm-up iterations
 
   XorecVersion xorec_version = XorecVersion::Scalar; ///< Version of XOR-EC to use (default: Scalar)
 
-  bool gpu_computation;                   ///< Flag for GPU computation
+  bool gpu_computation;                           ///< Flag for GPU computation
   size_t num_gpu_blocks = 0;
   size_t threads_per_gpu_block = 0;
 
@@ -43,10 +48,11 @@ struct BenchmarkConfig {
 using BenchmarkFunction = void(*)(benchmark::State&, const BenchmarkConfig&);
 
 /// @brief Constants for fixed values
-extern const std::vector<size_t> VAR_DATA_SIZES;
+constexpr size_t MESSAGE_SIZE = 128 MiB;
+constexpr size_t NUM_GPU_BLOCKS = 256;
+constexpr size_t NUM_THREADS_PER_BLOCK = 512;
+extern const std::vector<size_t> VAR_BLOCK_SIZES;
 extern const std::vector<ECTuple> VAR_EC_PARAMS;
+extern const std::vector<size_t> VAR_NUM_CPU_THREADS;
 extern const std::vector<size_t> VAR_NUM_LOST_BLOCKS;
-extern const std::vector<size_t> VAR_NUM_GPU_BLOCKS;
-extern const std::vector<size_t> VAR_NUM_THREADS_PER_BLOCK;
-
 #endif // BM_CONFIG_HPP
