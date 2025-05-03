f77220b6e7d45186008a1b7189040955457f9a5b
Kenji Yuri Nakano <kenji.y.nakano@gmail.com>
2025-05-03 16:36:19 +0200

    Implemented parallel Xorec version

--- src/algorithms/xorec_bm.cpp
+++ src/algorithms/xorec_bm.cpp
@@ -15,20 +15,44 @@ XorecBenchmark::XorecBenchmark(const BenchmarkConfig& config) noexcept
   : AbstractBenchmark(config),
     m_version(config.xorec_version)
 {
-  xorec_init(m_num_data_blocks);
+  xorec_init(m_chunk_data_blocks);
 }
 
 void XorecBenchmark::setup() noexcept {
-  std::fill_n(m_block_bitmap.get(), m_num_tot_blocks, 1);
+  std::fill_n(m_block_bitmap.get(), m_chunks * m_chunk_tot_blocks, 1);
   m_write_data_buffer();
+  omp_set_num_threads(m_threads);
 }
 
 int XorecBenchmark::encode() noexcept {
-  XorecResult res = xorec_encode(m_data_buf.get(), m_parity_buf.get(), m_block_size, m_num_data_blocks, m_num_parity_blocks, m_version);
-  return (res == XorecResult::Success) ? 0 : -1;
+  int return_code = 0;
+
+  #pragma omp parallel for
+  for (unsigned c = 0; c < m_chunks; ++c) {
+    auto data_buf = m_data_buf.get() + c * m_chunk_data_size;
+    auto parity_buf = m_parity_buf.get() + c * m_chunk_parity_size;
+
+    if (xorec_encode(data_buf, parity_buf, m_block_size, m_chunk_data_blocks, m_chunk_parity_blocks, m_version) != XorecResult::Success) {
+      #pragma omp atomic write
+      return_code = 1;
+    }
+  }
+  return return_code;
 }
 
 int XorecBenchmark::decode() noexcept {
-  XorecResult res = xorec_decode(m_data_buf.get(), m_parity_buf.get(), m_block_size, m_num_data_blocks, m_num_parity_blocks, m_block_bitmap.get(), m_version);
-  return (res == XorecResult::Success) ? 0 : -1;
+  int return_code = 0;
+
+  #pragma omp parallel for
+  for (unsigned c = 0; c < m_chunks; ++c) {
+    auto bitmap = m_block_bitmap.get() + c * m_chunk_tot_blocks;
+    auto data_buf = m_data_buf.get() + c * m_chunk_data_size;
+    auto parity_buf = m_parity_buf.get() + c * m_chunk_parity_size;
+
+    if (xorec_decode(data_buf, parity_buf, m_block_size, m_chunk_data_blocks, m_chunk_parity_blocks, bitmap, m_version) != XorecResult::Success) {
+      #pragma omp atomic write
+      return_code = 1;
+    }
+  }
+  return return_code;
 }
